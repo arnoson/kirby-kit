8a90731d718ebaed43deffc79898e484f41b3f34
Arno Schlipf <omasocke@hotmail.de>
2023-12-31 12:08:47 +0100

    fix: use correct extensions for glob import

--- site/scripts/index.ts
+++ site/scripts/index.ts
@@ -2,7 +2,7 @@ import { mountComponents } from '@very-simple/components'
 import { lazyLoad } from 'unlazy'
 import '~/styles/index.css'
 
-import.meta.glob('../(templates|snippets|layouts)/**/*.(ts|scss)', {
+import.meta.glob('../(templates|snippets|layouts)/**/*.(ts|css)', {
   eager: true,
 })
 
